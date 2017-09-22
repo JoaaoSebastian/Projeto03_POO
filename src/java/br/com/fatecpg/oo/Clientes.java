ad0fb9a3aa658298924af504356bcbd23f805206
João Victor <Joo Victor@DESKTOP-C11PSV5>
2017-09-22 18:07:09 -0300

    Entrada de dados

--- src/java/br/com/fatecpg/oo/Clientes.java
+++ src/java/br/com/fatecpg/oo/Clientes.java
@@ -9,6 +9,35 @@ public class Clientes {
    private String email;
    private String telefone;
    private String endereco;
+   
+   public static ArrayList<Clientes> list;
+public static ArrayList<Clientes> getList(){
+    if(list==null){
+    list = new ArrayList<>();
+    Clientes c1 =new Clientes();
+    c1.setNome("fulano");
+    c1.setRg("88.888.888-8");
+    c1.setEmail("fulano@gmail.com");
+    c1.setTelefone("13 9 8888-8888");
+    c1.setEndereco("Rua-888");
+    list.add(c1);
+    Clientes c2 =new Clientes();
+    c2.setNome("fulano");
+    c2.setRg("88.888.888-8");
+    c2.setEmail("fulano@gmail.com");
+    c2.setTelefone("13 9 8888-8888");
+    c2.setEndereco("Rua-888");
+    list.add(c2);
+    Clientes c3 =new Clientes();
+    c3.setNome("fulano");
+    c3.setRg("88.888.888-8");
+    c3.setEmail("fulano@gmail.com");
+    c3.setTelefone("13 9 8888-8888");
+    c3.setEndereco("Rua-888");
+    list.add(c3);
+    }
+return list;
+}
 
     public String getNome() {
         return nome;
--- src/java/br/com/fatecpg/oo/Fornecedores.java
+++ src/java/br/com/fatecpg/oo/Fornecedores.java
@@ -3,13 +3,44 @@ package br.com.fatecpg.oo;
 import java.util.ArrayList;
 
 public class Fornecedores {
-    
    private String nome;
    private String razao;
    private String cnpj;
    private String email;
    private String telefone;
    private String endereco;
+   
+   public static ArrayList<Fornecedores> list;
+public static ArrayList<Fornecedores> getList(){
+    if(list==null){
+    list = new ArrayList<>();
+    Fornecedores f1 =new Fornecedores();
+    f1.setNome("fulano");
+    f1.setRazao("razaofulano");
+    f1.setCnpj("88.888.888/0001-88");
+    f1.setEmail("fulano@gmail.com");
+    f1.setTelefone("13 9 8888-8888");
+    f1.setEndereco("Rua-888");
+    list.add(f1);
+    Fornecedores f2 =new Fornecedores();
+    f2.setNome("fulano");
+    f2.setCnpj("razaofulano");
+    f2.setCnpj("88.888.888/0001-88");
+    f2.setEmail("fulano@gmail.com");
+    f2.setTelefone("13 9 8888-8888");
+    f2.setEndereco("Rua-888");
+    list.add(f2);
+    Fornecedores f3 =new Fornecedores();
+    f3.setNome("fulano");
+    f3.setRazao("Razaofulano");
+    f3.setCnpj("88.888.888/0001-88");
+    f3.setEmail("fulano@gmail.com");
+    f3.setTelefone("13 9 8888-8888");
+    f3.setEndereco("Rua-888");
+    list.add(f3);
+    }
+return list;
+}
 
     public void setNome(String nome) {
         this.nome = nome;
